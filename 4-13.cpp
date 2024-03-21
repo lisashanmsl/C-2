1063a7f4db835928e5c648dddee94ce33b3ef30e
huaimi <wenglichen30316@gmail.com>
2024-03-21 22:10:33 +0800

    hw 4-13

---
+++ 4-13.cpp
@@ -0,0 +1,36 @@
+//overload function of mile per hour
+#include<iostream>
+using namespace std;
+
+//兩個不同版本的計算mph的function		
+double convertToMPH(int minute, int second);		//讀入配速幾分幾秒，轉換為每小時幾英哩
+double convertToMPH(double kph);					//讀入每小時幾公里，轉換為每小時幾英哩
+
+int main(void)
+{
+	int minute, second;	
+	double kph;
+	char choise;
+
+	do {
+		cout << "Enter minutes and seconds for pace in mph: ";
+		cin >> minute >> second;
+		cout << "MPH: " << convertToMPH(minute, second) << endl;
+
+		cout << "Enter kph to convert: ";
+		cin >> kph;
+		cout << "MPH: " << convertToMPH(kph) << endl;
+
+		cout << "Test again?(y/n): ";
+		cin >> choise;
+	} while (choise == 'y' || choise == 'Y');
+	
+	return 0;
+}
+double convertToMPH(int minute, int second) {
+	double total_minute = minute + (second / 60.0);
+	return(60.0 / total_minute);
+}
+double convertToMPH(double kph) {
+	return(kph / 1.61);
+}
