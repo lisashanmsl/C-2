0b2165157666062c58b3b9add475ce392dcc93e3
huaimi <wenglichen30316@gmail.com>
2024-03-22 18:26:36 +0800

    hw 5-7

--- 5-7.cpp
+++ 5-7.cpp
@@ -8,17 +8,18 @@ int search(const int a[], int numberUsed, int target);
 
 int main(void)
 {
-	int numberUsed;					//真正使用的陣列格數
+	int numberUsed;					//真正使用的陣列格數(陣列長度)
 	int grade[6] = { 0 };			//小考分數的範圍0~5，先初始化為0
 	int student_grade[SIZE];		//儲存使用者輸入的成績個數
 
+	//紀錄真正使用的陣列格數
 	fill_gradeTOarray(student_grade, SIZE, numberUsed);
 
 	//印出0~5的成績各有多少人
-	int count = 0;
+	int stdent_number;
 	for (int i = 0; i < 6; i++) {
-		count = search(student_grade, numberUsed, i);
-		cout << count << " grade(s) of " << i << endl;
+		stdent_number = search(student_grade, numberUsed, i);
+		cout << stdent_number << " grade(s) of " << i << endl;
 	}
 
 	return 0;
@@ -26,23 +27,26 @@ int main(void)
 void fill_gradeTOarray(int a[], int size, int& numberUsed) {
 	int grade;
 	int index = 0;
+
+	//輸入學生成績(0~5)直到輸入一個負數停止讀取
 	cout << "Enter number of student grade(0~5), stop enter by input -1: ";
 	cin >> grade;
 
 	while ((grade >= 0) && (index < SIZE)) {
-		a[index] = grade;
-		index++;
-		cin >> grade;
+		a[index] = grade;		//將讀入的成績存到陣列中
+		index++;				//更新索引值加一
+		cin >> grade;			//在讀下一個成績
 	}
 	cout << "\n";
-	numberUsed = index;
+	numberUsed = index + 1;		//紀錄真正使用到的陣列長度
 }
 int search(const int a[], int numberUsed, int target) {
-	int count = 0;
+	int count = 0;		//count用來記錄0~5分各有多少人
 	for (int i = 0; i < numberUsed; i++) {
+		//比較陣列中相同的分數，再用count紀錄有幾個相同的分數
 		if (a[i] == target) {
 			count++;
 		}
 	}
-	return(count);
+	return(count);		//回傳當前分數的總人數
 }
